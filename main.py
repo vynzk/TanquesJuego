e0ba3b063fe2effa552e8cc0f1f8968fba0fa924
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2021-09-03 03:22:16 -0400

    se agrega la verificación para los input cantJug y cantPart

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from Juego import *
 
-cantidadJugadores=int(input("Ingrese la cantidad de jugadores: "))
-cantidadPartidas=int(input("Ingrese la cantidad de partidas: "))
+while(True):
+    try:
+        cantidadJugadores=int(input("\nIngrese la cantidad de jugadores: "))
+        if(cantidadJugadores>1):
+            break       
+        else:
+            print("ERROR: la cantidad de jugadores debe ser mayor que 2, intentalo nuevamente")
+    except:
+        print("ERROR: variable de tipo incorrecto")
+
+while(True):
+    try:        
+        cantidadPartidas=int(input("\nIngrese la cantidad de partidas: "))
+        if(cantidadPartidas>=1):
+            break;
+        else:
+            print("ERROR: la cantidad de partidas debe ser mayor o igual a 1, intentalo nuevamente")
+    except:
+        print("ERROR: variable de tipo incorrecto")        
+
 game=Juego(cantidadJugadores,cantidadPartidas)
 game.mostrarCaracteristicas()
 game.registroJugadores()
