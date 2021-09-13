9ca0cc2436075b39a73694d4341eff0c31bd139a
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2021-09-13 14:26:02 -0500

    cada tanque posee un obj cuadrado representado en el mapa
    cada tanque al crearse crea un objeto bloque el cual se guardará en el atributo self.cuadrado. Este cuadrado se utilizará para efectuar el disparo (inicia desde las posiciones de este)

--- GUI/director.py
+++ GUI/director.py
@@ -6,27 +6,27 @@ import pygame
 class Director:
     """El director se encarga de iniciar el juego,
         cambiar las escenas y recoger e interpretar los eventos de estas."""
-    
-    def __init__(self): #constructor
-        
-        self.pantalla = pygame.display.set_mode((1280,720))
+
+    def __init__(self):  # constructor
+
+        self.pantalla = pygame.display.set_mode((1280, 720))
         self.escena = None
         self.running = True
-        self.listaEscenas= []
-        self.mousePos= None
-        #self.mousex,self.mousey= 0,0 #para movimiento del mouse
-        #self.enlistarEscenas() #quizas se implemente en necesidad.
-    
-    def gameLoop(self): #bucle del juego
+        self.listaEscenas = []
+        self.mousePos = None
+        # self.mousex,self.mousey= 0,0 #para movimiento del mouse
+        # self.enlistarEscenas() #quizas se implemente en necesidad.
+
+    def gameLoop(self):  # bucle del juego
         while self.running:
-            #pygame.display.set_caption("PYTHON TANKS") #si lo activas hay un bug re loco... no lo hagas :-)
-            #Evento de salida
+            # pygame.display.set_caption("PYTHON TANKS") #si lo activas hay un bug re loco... no lo hagas :-)
+            # Evento de salida
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     print('Juego cerrado')
                     self.running = False
-                self.escena.on_event(event) #no mover
-        
+                self.escena.on_event(event)  # no mover
+
             """ # for partida de Luis
                 for partida in self.juego.getListaPartidas():
                     self.cambiarEscena(partida.getEscena())
@@ -50,22 +50,19 @@ class Director:
             self.escena.on_draw(self.pantalla)
             pygame.display.update()
 
-#---------- funciones kernel (avisar si la tocan) ----------#
+    # ---------- funciones kernel (avisar si la tocan) ----------#
     def cambiarEscena(self, escenaNueva):
         "cambia la escena actual"
-        self.escena = escenaNueva  
-    def checaBoton(self , mousePos, botonNombre): #detecta el tocar un boton
+        self.escena = escenaNueva
+
+    def checaBoton(self, mousePos, botonNombre):  # detecta el tocar un boton
         if botonNombre.rect.collidepoint((mousePos)):
             return True
 
-            
-
-# ---- funciones de luis ---
+    # ---- funciones de luis ---
     def terminoJuego(self):
         # cambiarEscena(escenaCreditos) algo así
         self.juego.mostrarRanking()
         self.juego.definirGanador()
-        print("\n!!!!!!!!!!! El/la ganadora es: ",self.juego.getJugadorGanador().getNombre()," !!!!!!!!!!!!!!")
+        print("\n!!!!!!!!!!! El/la ganadora es: ", self.juego.getJugadorGanador().getNombre(), " !!!!!!!!!!!!!!")
         print("terminó el juego")
-    
-
