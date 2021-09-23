4042c1f6332c287f3e3ef3b8e6d5be54cecff59a
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2021-09-23 20:58:42 -0300

    modificación (martin), se hace una funcion dentro de juego randomica

--- Videojuego/Juego.py
+++ Videojuego/Juego.py
@@ -13,31 +13,30 @@ class Juego:
         self.jugadorGanador = None
         self.juegoTerminado=False
 
-    def agregarJugador(self, i, pantalla):
-        # colores de los tanques
-
-        # posiciones posibles del primer tanque 
-        posiciones1 = [(0, 520), (20, 520), (40, 520), (60, 520), (80, 520), (100, 520), 
+    def posicionesRandom(self,i):
+        # posiciones posibles para el primer tanque
+        posPosiblesJ1 = [(0, 520), (20, 520), (40, 520), (60, 520), (80, 520), (100, 520), 
         (120, 500), (140, 500), (160, 480), (180, 460), (200, 440), (220, 440), (240, 440),
         (260, 440), (280, 440), (300, 460), (320, 480), (340, 520)]
 
         # posiciones posibles del segundo tanque
-        posiciones2 = [(940, 500), (960, 500), (980, 480), (1000, 460), 
+        posPosiblesJ2 = [(940, 500), (960, 500), (980, 480), (1000, 460), 
         (1020, 460), (1040, 440), (1060, 440), (1080, 440), (1100, 440), (1120, 420), (1140, 420), 
         (1160, 420), (1180, 420), (1200, 420), (1220, 420), (1240, 420), (1260, 420), (1280, 420)]
 
-        # randomizado por separado de la posicion de los tanques eespetando la mitad del mapa o más de separación
-        delta = random.randint(1, 18)
-
-        if i == 1:
-            posiciones = posiciones1[delta-1]
-        if i == 2:
-            posiciones = posiciones2[delta-1]
-
+        numRandom=random.randint(0,17)
+        # retorna el par ordenado posible para cada jugador
+        if i==1:
+            return posPosiblesJ1[numRandom]
+        else:
+            return posPosiblesJ2[numRandom]
+            
+    def agregarJugador(self, i, pantalla):
+        # colores de los tanques
         colores = [(255, 0, 0), (0, 0, 255)]
-        posiciones = [(20, 520), (1200, 420)]
+        posicion=self.posicionesRandom(i)
         nombre = str(input("Ingrese su nombre: "))
-        tanque = Tanque(pantalla, 20, 20, colores[i - 1], posiciones[0], posiciones[1])
+        tanque = Tanque(pantalla, 20, 20, colores[i - 1], posicion[0], posicion[1])
         self.listaJugadores.append(Jugador(nombre, tanque))  # << agrega un nuevo Jugador con su nombre y su tanque
 
     # función que se encargará de llenar la lista de jugadores, registrará tantos jugadores
