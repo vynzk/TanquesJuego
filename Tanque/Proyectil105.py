06bd30c3cb3bd4113e25830011908f769d244a73
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2021-10-13 21:33:57 -0300

    se añade color y miniatura a cada proyectil
    preferí sólo mostrar un color respectivo para c/u de los proyectiles siendo ROJO el proyectil de 105, NARANJA el perforante y AMARILLO el de 60.
    
    Además, c/u de ellos tiene una imagen la cual puede ser mostrada a la hora de cambiar en la GUI de cambio de arma.

--- Tanque/Proyectil105.py
+++ Tanque/Proyectil105.py
@@ -1,8 +1,13 @@
+import pygame
+from GUI.colores import *
+
 from Tanque.Proyectil import *
 
 class Proyectil105(Proyectil):
     def __init__(self, daño, stock):
         Proyectil.__init__(self, daño, stock)
+        self.imagen=pygame.image.load("GUI/imagenes/armas/proyectil105.png")
+        self.color=ROJO
 
     # futura construcción
     def efectoDestructivo(self):
