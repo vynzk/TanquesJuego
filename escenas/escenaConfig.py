d170d38c3fd469738ae966c9a356b873cdc94d60
yanez valentina <vyanez20@alumnos.utalca.cl>
2021-11-04 15:06:38 -0300

    creada e implementandose: escena configuraciones

---
+++ escenas/escenaConfig.py
@@ -0,0 +1,72 @@
+import pygame
+from escenas import plantillaEscena
+from utilidades.Boton import Boton
+from utilidades.colores import *
+
+class EscenaConfig(plantillaEscena.Escena):
+
+    def __init__(self, director):
+        plantillaEscena.Escena.__init__(self, director)
+        self.director = director
+        self.fondo= pygame.image.load("imagenes/fondoHome.png")
+        # botones
+        self.boton_aplicar = None
+        self.boton_restablecer = None
+        self.boton_numJugadores = None
+        self.boton_numIa = None
+        self.boton_afectosEntorno = None
+        self.boton_dimensionPantalla = None
+            #cant muiniciones
+        self.boton_perforante = None
+        self.boton_105mm = None
+        self.boton_60mm = None
+
+    
+    def on_update(self):
+        pygame.display.set_caption("configuraciones")
+
+    def on_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            self.director.mousePos = pygame.mouse.get_pos()
+            if self.director.checaBoton(self.director.mousePos, self.boton_aplicar):
+                print('ok')
+            if self.director.checaBoton(self.director.mousePos, self.boton_restablecer):
+                print('ok')       
+        
+
+    def on_draw(self, pantalla):
+        botonVacio= pygame.image.load("imagenes/botones/botonVacio.png")
+
+        pantalla.blit(self.fondo, (0,0))
+        
+        
+        self.boton_aplicar = Boton(pantalla, "play", 64, 420,botonVacio,40,40)
+        self.boton_aplicar.dibujaBoton()
+
+        self.boton_restablecer = Boton(pantalla, "play", 1200, 420,botonVacio,40,40)
+        self.boton_restablecer.dibujaBoton()
+
+        self.textoEnPantalla(f' cantidad de jugadores',15,BLANCO,(114,150),False)
+        self.boton_numJugadores= Boton(pantalla, "play", 64, 150,botonVacio,40,40)
+        self.boton_numJugadores.dibujaBoton()
+
+        self.textoEnPantalla(f' cantidad de jugadores IA',15,BLANCO,(114,200),False)
+        self.boton_numIa = Boton(pantalla, "play", 64, 200,botonVacio,40,40)
+        self.boton_numIa.dibujaBoton()
+
+        self.textoEnPantalla(f' efectos de entorno?',15,BLANCO,(114,250),False)
+        self.boton_afectosEntorno = Boton(pantalla, "play", 64, 250,botonVacio,40,40)
+        self.boton_afectosEntorno.dibujaBoton()
+
+        self.textoEnPantalla(f' dimension de pantalla',15,BLANCO,(214,300),False)
+        self.boton_dimensionPantalla = Boton(pantalla, "play", 64, 300,botonVacio,127,40)
+        self.boton_dimensionPantalla.dibujaBoton()
+        #municion
+        self.boton_perforante = Boton(pantalla, "play", 900, 150,botonVacio,40,40)
+        self.boton_perforante.dibujaBoton()   
+
+        self.boton_105mm = Boton(pantalla, "play", 900, 200,botonVacio,40,40)
+        self.boton_105mm.dibujaBoton()  
+
+        self.boton_60mm = Boton(pantalla, "play", 900, 250,botonVacio,40,40)
+        self.boton_60mm.dibujaBoton()       
--- escenas/escenaHome.py
+++ escenas/escenaHome.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from escenas.escenaRegistro import EscenaRegistro
+from escenas.escenaConfig import EscenaConfig
 import pygame
 from escenas import plantillaEscena
 from utilidades.Boton import Boton
@@ -11,6 +12,7 @@ class EscenaHome(plantillaEscena.Escena):
     def __init__(self, director):  # constructor
         plantillaEscena.Escena.__init__(self, director)
         self.boton_play = None
+        self.boton_config= None
         self.fondo= pygame.image.load("imagenes/fondoHome.png")
         self.cambiaDePartida()
 
@@ -23,6 +25,8 @@ class EscenaHome(plantillaEscena.Escena):
             self.director.mousePos = pygame.mouse.get_pos()
             if self.director.checaBoton(self.director.mousePos, self.boton_play):
                 self.cambiaDePartida()
+            if self.director.checaBoton(self.director.mousePos, self.boton_config):
+                self.cambiaConfiguracion()
 
     """Esta función corresponde a lo mostrado en pantalla: usada en director.py"""
 
@@ -31,6 +35,10 @@ class EscenaHome(plantillaEscena.Escena):
         botonJugar= pygame.image.load("imagenes/botones/botonJugar.png")
         self.boton_play = Boton(pantalla, "play", 540, 420,botonJugar,127,40)
         self.boton_play.dibujaBoton()
+        self.boton_config = Boton(pantalla, "configuracion", 540, 470,botonJugar,127,40)
+        self.boton_config.dibujaBoton()
 
     def cambiaDePartida(self):
         self.director.cambiarEscena(EscenaRegistro(self.director))
+    def cambiaConfiguracion(self):
+        self.director.cambiarEscena(EscenaConfig(self.director))
