b109f3341c97be107a688d373a74c0c97289fd35
yanez valentina <vyanez20@alumnos.utalca.cl>
2021-12-10 08:13:04 -0600

    repuestas lineas faltantes escena config

--- escenas/escenaConfig.py
+++ escenas/escenaConfig.py
@@ -304,7 +304,15 @@ class EscenaConfig(plantillaEscena.Escena):
         self.indicarClima = "Desactivado"
 
         # valores predeterminados
-        
+        self.numJugadores= 2
+        self.afectosEntorno = 'no'
+        self.cajaDimensionY.texto = "800"
+        self.cajaDimensionX.texto = "800"
+        self.cajaPerforante.texto = "10"
+        self.caja100mm.texto= "10"
+        self.caja60mm.texto= "10"
+        self.cajaGravedad.texto= "9.8"
+
 
         self.cajaDimensionY_valor = 800
         self.cajaDimensionX_valor = 800
